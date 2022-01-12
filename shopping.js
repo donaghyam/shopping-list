3e9879a076f9625e833ac57466bf055b39849ce6
Adam Donaghy <donaghyam@gmail.com>
2022-01-12 12:01:58 -0600

    adding list

---
+++ shopping.js
@@ -0,0 +1,19 @@
+const list = [
+    {
+        id: 1,
+        item: "milk",
+        price: 4
+    },
+    {
+        id: 2,
+        item: "eggs",
+        price: 5
+    },
+    {
+        id: 3,
+        item: "bread",
+        price: 3
+    }
+]
+
+console.log(list)
